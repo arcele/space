200ff06a376af9604c1deab5eaa615a35e841f4a
J. Ryan Kelley <ryan.kelley@espn.com>
2015-05-12 22:05:11 -0700

    basic space exploration viewer with random discoveries

---
+++ js/space.js
@@ -0,0 +1,26 @@
+$(document).ready(
+	function() {
+		setTimeout(function() {
+			$(".loading").addClass('faded');
+			for(var i = 0; i < 25; i++) { // 5 Random stars for now
+				var top = Math.random() * 300;
+				var left = Math.random() * 875;
+				var star = $('<div class="' +  (Math.round(Math.random() * 10) % 4 == 0 ? 'star' : 'unknown') + '" style="top:' + top +'px;left:' + left +'px;"></div>');
+				$("#space").append(star);
+			}
+		//	$("#space .star").addClass("advance");
+		}, 800)
+
+		setTimeout(function() {
+			$("#space .star").addClass("advance");
+		}, 1075)
+
+		setTimeout(function() {
+			$("#space").append($('<div class="complete faded"></div>'));
+		},31875)
+
+		setTimeout(function() {
+			$(".complete").removeClass('faded');
+		},32000);
+	}
+);
