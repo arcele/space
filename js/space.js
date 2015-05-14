6405c2a7a540c146cac02ea90223f987e2e5b3f1
J. Ryan Kelley <ryan.kelley@espn.com>
2015-05-13 23:53:32 -0700

    set the id that we want to use

--- js/space.js
+++ js/space.js
@@ -5,6 +5,7 @@ $(document).ready(
 			for(var i = 0; i < 25; i++) { // 5 Random stars for now
 				var top = Math.random() * 300;
 				var left = Math.random() * 875;
+				var eId = 'space-it-' + i + Math.round(Math.random() * 1000);
 				var star = $('<div id="'+eId+'" class="' +  (Math.round(Math.random() * 10) % 4 == 0 ? 'star' : 'unknown') + '" style="top:' + top +'px;left:' + left +'px;"></div>');
 				$("#space").append(star);
 			}
