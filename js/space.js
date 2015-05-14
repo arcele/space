d5cb58aff605fc23578d4e56824d87add807d354
J. Ryan Kelley <ryan.kelley@espn.com>
2015-05-13 20:49:02 -0700

    give all of the stars/uknowns ids

--- js/space.js
+++ js/space.js
@@ -5,7 +5,7 @@ $(document).ready(
 			for(var i = 0; i < 25; i++) { // 5 Random stars for now
 				var top = Math.random() * 300;
 				var left = Math.random() * 875;
-				var star = $('<div class="' +  (Math.round(Math.random() * 10) % 4 == 0 ? 'star' : 'unknown') + '" style="top:' + top +'px;left:' + left +'px;"></div>');
+				var star = $('<div id="'+eId+'" class="' +  (Math.round(Math.random() * 10) % 4 == 0 ? 'star' : 'unknown') + '" style="top:' + top +'px;left:' + left +'px;"></div>');
 				$("#space").append(star);
 			}
 		//	$("#space .star").addClass("advance");
