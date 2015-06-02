fd42872439029ab2a15ba2e2e79f4004eccceddd
J. Ryan Kelley <ryan.kelley@espn.com>
2015-06-01 21:40:00 -0700

    stars and unknowns both move in regards to exploration vector.

--- js/space.js
+++ js/space.js
@@ -23,6 +23,10 @@ $(document).ready(
 				var radius = -1;
 				if(className == 'star') {
 					radius = 1.85 + Math.random() * 3;
+				} else if(className == 'unknown') {
+					radius = 1 + Math.random();
+				} else {
+					radius = Math.random();
 				}
 				this.items.push($('<div id="'+eId+'" class="item ' +  className + '" style="top:' + top +'px;left:' + left +'px;" data-radius="'+radius+'"></div>'));
 			}
@@ -52,37 +56,46 @@ $(document).ready(
 
 		Space.prototype.explore = function(e) {
 			this.explorationVector = e;
-			var starMultiplier = 10;
+			var itemMultipler, pos, deltaX, deltaY, distance, angle, linearVelocity, newTop, newLeft;
 			for(var i = 0; i < this.items.length; i++) {
+				// Determine item's "distance" from explorationVector
+				pos = this.items[i].position();
+				deltaX = pos.left - e.clientX;
+				deltaY = pos.top - e.clientY;
+				distance = Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2)); // quadradic, thanks pre-algebra
+				// Determine star's angle from explorationVector
+				angle = Math.atan(deltaY / deltaX);
 				if(this.items[i].hasClass('star')) {
-					// Determine star's "distance" from explorationVector
-					var pos = this.items[i].position();
-					var deltaX = pos.left - e.clientX;
-					var deltaY = pos.top - e.clientY; 
-					var distance = Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2)); // quadradic, thanks pre-algebra
-					// Determine star's angle from explorationVector
-					var angle = Math.atan(deltaY / deltaX);
-					var linearVelocity = Math.pow(distance, 2) / 500; // The further from the exploration point the star, the faster it diverges from the current view
-
-					// This could be improved with some trig
-					var newTop, newLeft;
-					if(deltaY < 0) {
-						newTop = pos.top - linearVelocity;
-					} else {
-						newTop = pos.top + linearVelocity;
-					}
-					if(deltaX < 0) {
-						newLeft = pos.left - linearVelocity;
-					} else {
-						newLeft = pos.left + linearVelocity;
-					}
-					var finalRadius = this.items[i].data('radius') * starMultiplier;
-					this.items[i].css('transition', 'width 30s linear, height 30s linear, top 30s linear, left 30s linear')
-						.css('width',  finalRadius +'px')
-						.css('height', finalRadius + 'px')
-						.css('top', newTop)
-						.css('left', newLeft);
+					linearVelocity = Math.pow(distance, 2) / 500; // The further from the exploration point the star, the faster it diverges from the current view
+					itemMultiplier = 10
+				} else if(this.items[i].hasClass('unknown')) {
+					console.log('vel:', linearVelocity);
+					itemMultiplier = 9
+			    } else {
+					linearVelocity = 1;//Math.pow(distance, 2) / 5000;
+					itemMultiplier = 3
 				}
+
+				// This could be improved with some trig
+				if(deltaY < 0) {
+					newTop = pos.top - linearVelocity;
+				} else {
+					newTop = pos.top + linearVelocity;
+				}
+				if(deltaX < 0) {
+					newLeft = pos.left - linearVelocity;
+				} else {
+					newLeft = pos.left + linearVelocity;
+				}
+				var finalRadius = this.items[i].data('radius') * itemMultiplier;
+
+				console.log(this.items[i], finalRadius, newTop, newLeft);
+				this.items[i].css('transition', 'width 30s linear, height 30s linear, top 30s linear, left 30s linear')
+					.css('width',  finalRadius +'px')
+					.css('height', finalRadius + 'px')
+					.css('top', newTop)
+					.css('left', newLeft);
+
 			}
 
 			$("#space").addClass("advance");
